f21c16ae6b7e6f307d07a3f7cd67d34740fd9b74
Diondre Edwards <diondre27@hotmail.com>
2017-06-07 17:43:11 -0400

    initial commit

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>@yield('title')</title>
+
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="{{ URL::to('src/css/main.css') }}">
+        <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=AIzaSyDhBqp9zJhglqielGt3eJIeAtK-bzDxjgw&sensor=false"></script>
+    </head>
+    <body>
+    	@include('includes.header')
+    	<div class="container">
+    		@yield('content')
+    	</div>
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <script src="{{ URL::to('src/js/app.js') }}"></script>
+    </body>
+</html>
